3f0b137c9bd66323725d7c087a6556f5d5cde7cf
Curtis <curtismanagh@hotmail.co.uk>
2025-04-02 15:06:44 +0100

    Added plugin to track when target will be unfrozen from ZGS spec

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/zgsfreezetimer/ZGSFreezeTimerConfig.java
@@ -0,0 +1,8 @@
+package net.runelite.client.plugins.zgsfreezetimer;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("zgsfreezetimer")
+public interface ZGSFreezeTimerConfig extends Config {
+}
